4a8941b3495486c652301eed12086f955713768a
Malcolm Roberts <malcolmiwroberts@gmail.com>
2016-05-21 16:01:33 +0200

    Indent using R-mode from ess.

--- timing.R
+++ timing.R
@@ -7,32 +7,30 @@ filename <- 'cconv3_implicit'
 filetodf <- function(filename)
 {
 # Read data from file, dropping first row
-dat <- readLines(filename)[-1]
+    dat <- readLines(filename)[-1]
 
 # Split each line treating 'one or more spaces' as delimiter
-dat <- strsplit(dat, '\\s+')
+    dat <- strsplit(dat, '\\s+')
 
 # Convert to numeric
-dat <- lapply(dat, as.numeric)
+    dat <- lapply(dat, as.numeric)
 
 # Creat the data frame and fill it
-print( "times" )
-times<-NULL;
-cnames <- c(1:length(dat))
-for( i in 1:length(dat) )
-{
-	print(i)
-	m <- dat[i][[1]][[1]]
-	print(m)
-	cnames[i] <- toString(m)
-	row <- dat[i][[1]][c(3:lengths(dat[i]))]
-	print(row)
-	cbind(times, row) -> times
-
-#times <- data.frame(dat[m][[1]][c(3:lengths(dat[i]))])
-}
-colnames( times ) <- cnames
-return( times )
+    print( "times" )
+    times<-NULL;
+    cnames <- c(1:length(dat))
+    for( i in 1:length(dat) )
+        {
+            print(i)
+            m <- dat[i][[1]][[1]]
+            print(m)
+            cnames[i] <- toString(m)
+            row <- dat[i][[1]][c(3:lengths(dat[i]))]
+            print(row)
+            cbind(times, row) -> times
+        }
+    colnames( times ) <- cnames
+    return( times )
 }
 
 times <- filetodf(filename)
@@ -41,7 +39,6 @@ times <- filetodf(filename)
 typeof(times)
 times
 summary(times)
-
 
 # Compare the m=64 times with the m=128 times
 wilcox.test(times[,"64"], times[,"128"])
